4d211ea4b0e4747ea8af3d4b507c0afd5aad9927
alfarel <alfarelcynthesis@proton.me>
2025-10-31 19:46:43 +0000

    WIP notes/obsidian-nvim: remove unnecessary options and add useful ones

--- modules/plugins/notes/obsidian/config.nix
+++ modules/plugins/notes/obsidian/config.nix
@@ -4,6 +4,7 @@
   ...
 }: let
   inherit (lib.modules) mkIf;
+  inherit (lib.generators) mkLuaInline;
   inherit (lib.nvim.dag) entryAnywhere;
   inherit (lib.nvim.binds) pushDownDefault;
   inherit (lib.nvim.lua) toLuaObject;
@@ -25,6 +26,17 @@ in {
       pluginRC.obsidian = entryAnywhere ''
         require("obsidian").setup(${toLuaObject cfg.setupOpts})
       '';
+
+      # Resolve markdown image paths in the vault.
+      utility.snacks-nvim.setupOpts = mkIf (config.vim.utility.snacks-nvim.setupOpts.image.enabled or false) {
+        image.resolve = mkLuaInline ''
+          function(path, src)
+            if require("obsidian.api").path_is_note(path) then
+              return require("obsidian.api").resolve_image_path(src)
+            end
+          end
+        '';
+      };
     };
   };
 }
--- modules/plugins/notes/obsidian/obsidian.nix
+++ modules/plugins/notes/obsidian/obsidian.nix
@@ -4,11 +4,9 @@
   ...
 }: let
   inherit (lib.options) mkEnableOption mkOption;
-  inherit (lib.types) bool str nullOr;
-  inherit (lib.modules) mkRenamedOptionModule;
+  inherit (lib.types) enum nullOr;
+  inherit (lib.modules) mkRenamedOptionModule mkRemovedOptionModule;
   inherit (lib.nvim.types) mkPluginSetupOption;
-
-  autocompleteCfg = config.vim.autocomplete;
 in {
   imports = let
     renamedSetupOption = oldPath: newPath:
@@ -16,36 +14,82 @@ in {
       (["vim" "notes" "obsidian"] ++ oldPath)
       (["vim" "notes" "obsidian" "setupOpts"] ++ newPath);
   in [
-    (renamedSetupOption ["dir"] ["dir"])
+    (
+      mkRemovedOptionModule ["vim" "notes" "obsidian" "dir"]
+      ''
+        `obsidian.nvim` has migrated to the `setupOpts.workspaces` option to support multiple vaults with a single interface.
+
+        To continue using a single vault, set:
+
+        ```nix
+        {
+          notes.obsidian.setupOpts.workspaces = [
+            {
+              name = "any-string";
+              path = "~/old/dir/path/value";
+            }
+          ];
+        }
+        ```
+      ''
+    )
     (renamedSetupOption ["daily-notes" "folder"] ["daily_notes" "folder"])
     (renamedSetupOption ["daily-notes" "date-format"] ["daily_notes" "date_format"])
     (renamedSetupOption ["completion"] ["completion"])
   ];
   options.vim.notes = {
     obsidian = {
-      enable = mkEnableOption "complementary neovim plugins for Obsidian editor";
+      enable = mkEnableOption ''
+        plugins to compliment the Obsidian markdown editor [obsidian.nvim].
 
-      setupOpts = mkPluginSetupOption "Obsidian.nvim" {
-        daily_notes = {
-          folder = mkOption {
-            type = nullOr str;
-            default = null;
-            description = "Directory in which daily notes should be created";
-          };
-          date_format = mkOption {
-            type = nullOr str;
-            default = null;
-            description = "Date format used for creating daily notes";
-          };
-        };
+        :: [!tip] Folding
+        :: This plugin depends on [vim-markdown] which by default folds headings,
+        :: including outside of workspaces/vaults.
+        ::
+        :: Set `vim.g['vim_markdown_folding_disable'] = 1` to disable automatic folding,
+        :: or `vim.g['vim_markdown_folding_level'] = <number>` to set the default folding level.
 
-        completion = {
-          nvim_cmp = mkOption {
-            # If using nvim-cmp, otherwise set to false
-            type = bool;
-            description = "If using nvim-cmp, otherwise set to false";
-            default = autocompleteCfg.nvim-cmp.enable || autocompleteCfg.blink-cmp.enable;
-          };
+        :: [!note] Completion
+        :: This plugin will automatically use [blink-cmp] (preferred) or [nvim-cmp] for completion
+      '';
+
+      # TODO: test the suggested global options
+
+      # TODO: check option rendering
+      # do I need to link plugins?
+      # does :: [!note] work?
+
+      # TODO: make sure this actually builds, options work etc.
+
+      setupOpts = mkPluginSetupOption "obsidian.nvim" {
+        # # TODO: docs say it'll disable itself automatically, check if this is true
+        # ui.enable = let
+        #   markdownExtensions = config.vim.languages.markdown.extensions;
+        # in
+        #   mkEnableOption "[obsidian.nvim] UI rendering"
+        #   // {
+        #     default = !(markdownExtensions.render-markdown-nvim.enable || markdownExtensions.markview-nvim.enable);
+        #     defaultText = "false if render-markdown-nvim or markview-nvim are enabled, otherwise true";
+        #   };
+
+        # The plugin doesn't choose or detect this.
+        # It's a good default to use an enabled plugin.
+        picker.name = mkOption {
+          # From https://github.com/obsidian-nvim/obsidian.nvim/blob/main/lua/obsidian/config/init.lua
+          type = nullOr (enum ["snacks.pick" "mini.pick" "telescope.nvim" "fzf-lua"]);
+          default =
+            if (config.vim.utility.snacks-nvim.setupOpts.picker.enabled or false)
+            then "snacks.pick"
+            else if config.vim.mini.pick.enable
+            then "mini.pick"
+            else if config.vim.telescope.enable
+            then "telescope.nvim"
+            else if config.vim.fzf-lua.enable
+            then "fzf-lua"
+            else null;
+          defaultText = ''
+            One of "snacks", "mini", "telescope", "fzf_lua", or null based on whether they are enabled and in that order.
+          '';
         };
       };
     };
